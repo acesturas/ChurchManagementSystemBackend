81474182fc8e0a3758095ab314307793c54afcbd
Timothy <timothyaraojoesturas@gmail.com>
2026-08-01 20:35:14 +0800

    add a debug to check the google script url

--- src/main/java/tim/dev/gfs/google/client/GoogleSheetsClient.java
+++ src/main/java/tim/dev/gfs/google/client/GoogleSheetsClient.java
@@ -4,16 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import jakarta.annotation.PostConstruct;
 import tim.dev.gfs.dto.GoogleSheetRequest;
 
 @Component
 public class GoogleSheetsClient {
-
+	
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Value("${google.sheets.url}")
     private String scriptUrl;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Google Script URL: " + scriptUrl);
+    }
 //    public <ResponseType, RequestData> ResponseType post(GoogleSheetRequest<RequestData> request, Class<ResponseType> responseType) {
 //
 //        return restTemplate.postForObject(
